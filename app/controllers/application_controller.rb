4fc553d791f5050c3ca8eb9eba42a99e825b9f48
Michelle Olivieri <michelleo72@gmail.com>
2018-05-15 23:44:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,11 +15,9 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-    @recipe.save
+
     redirect '/recipes/#{@recipe.id}'
   end
-
-
 
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
@@ -28,16 +26,14 @@ class ApplicationController < Sinatra::Base
 
   get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
-    erb :show
+    erb :edit
   end
 
   patch '/recipes/:id' do
 
   end
 
-#  get '/' do
 
-#  end
 
   delete '/recipes/:id/delete' do
 
