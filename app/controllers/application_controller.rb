80f263fe4c02b8ade0fc798680c63d48d3283349
Michelle Olivieri <michelleo72@gmail.com>
2018-05-15 23:46:37 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,6 +34,8 @@ class ApplicationController < Sinatra::Base
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect '/'
   end
 
   delete '/recipes/:id/delete' do
